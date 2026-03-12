9291bd6d1210c224b6990e76b1a1c4e9ea6be1bf
Jobulikannan <kannan@jobuli.com>
2026-03-13 05:18:52 +0530

    props types and tips completed
    props types and tips done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Container } from "./components/Container";
+import { Input } from "./components/Input";
 
 function App() {
   return (
     <>
       <div>
         <Container styles={{ border: "1px solid black", padding: "1rem" }} />
+        <Input value="" handleChange={(event) => console.log(event)} />
       </div>
     </>
   );
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,9 +1,4 @@
-type PersonProps = {
-  name: {
-    first: string;
-    last: string;
-  };
-};
+import type { PersonProps } from "./Person.types";
 
 export const Person = (props: PersonProps) => {
   return (
