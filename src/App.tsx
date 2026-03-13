c94927e03743023cf7cb2b1172f7c4f5ad0c241d
Jobulikannan <kannan@jobuli.com>
2026-03-13 05:37:31 +0530

    useState addressed
    useState done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "./App.css";
-import { Container } from "./components/Container";
-import { Input } from "./components/Input";
+import { LoggedIn } from "./components/state/LoggedIn";
 
 function App() {
   return (
     <>
       <div>
-        <Container styles={{ border: "1px solid black", padding: "1rem" }} />
-        <Input value="" handleChange={(event) => console.log(event)} />
+        <LoggedIn />
       </div>
     </>
   );
